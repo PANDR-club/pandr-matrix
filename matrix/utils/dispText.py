74451f02fe1fabf2fc9d7f6587b7c6e1a8908963
Bruno Greenfield <brunogreenfielduk@gmail.com>
2025-01-06 19:50:18 +0000

    PAN-22: Improvements + Custom args

--- matrix/utils/dispText.py
+++ matrix/utils/dispText.py
@@ -1,16 +1,25 @@
-from matrix import matrix, consts
+from matrix import consts, matrixSetup
 from rgbmatrix import graphics
+import argparse
+
+# Flags
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--text', required=True)
+parser.add_argument('-x', '--posX', required=True)
+parser.add_argument('-y', '--posY', required=True)
+
+args = parser.parse_args()
 
 # Displays given text to the matrix
 def dispText(text, posx, posy, colour, font=consts.largeFont):
-    matrix.canvas.Clear()
-    graphics.DrawText(matrix.canvas, font, posx, posy, colour, text)
-    matrix.canvas = matrix.rgbMatrix.SwapOnVSync(matrix.canvas)
+    matrixSetup.canvas.Clear()
+    graphics.DrawText(matrixSetup.canvas, font, posx, posy, colour, text)
+    matrixSetup.canvas = matrixSetup.rgbMatrix.SwapOnVSync(matrixSetup.canvas)
 
 if __name__ == "__main__":
     try:
         while True:
-            dispText(matrix.args.text, int(matrix.args.posX), int(matrix.args.posY), consts.RED)
+            dispText(args.text, int(args.posX), int(args.posY), consts.RED)
 
     except KeyboardInterrupt:
-        matrix.rgbMatrix.Clear()
+        matrixSetup.rgbMatrix.Clear()

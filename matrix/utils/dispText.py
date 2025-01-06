cbc26c153d7f2f5977e955e7aa39beed29dfb014
Bruno Greenfield <brunogreenfielduk@gmail.com>
2025-01-06 20:47:17 +0000

    Move flags into main checked area

--- matrix/utils/dispText.py
+++ matrix/utils/dispText.py
@@ -2,13 +2,6 @@ from matrix import consts, matrixSetup
 from rgbmatrix import graphics
 import argparse
 
-# Flags
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--text', required=True)
-parser.add_argument('-x', '--posX', required=True)
-parser.add_argument('-y', '--posY', required=True)
-
-args = parser.parse_args()
 
 # Displays given text to the matrix
 def dispText(text, posx, posy, colour, font=consts.largeFont):
@@ -17,6 +10,14 @@ def dispText(text, posx, posy, colour, font=consts.largeFont):
     matrixSetup.canvas = matrixSetup.rgbMatrix.SwapOnVSync(matrixSetup.canvas)
 
 if __name__ == "__main__":
+    # Flags
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--text', required=True)
+    parser.add_argument('-x', '--posX', required=True)
+    parser.add_argument('-y', '--posY', required=True)
+
+    args = parser.parse_args()
+
     try:
         while True:
             dispText(args.text, int(args.posX), int(args.posY), consts.RED)

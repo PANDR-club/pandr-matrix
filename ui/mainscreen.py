21168c7ee6f8c755fffa07932f90bc9d21dd1fda
Bruno Greenfield <brunogreenfielduk@gmail.com>
2025-01-28 16:14:24 +0000

    MERGE Main

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -36,6 +36,33 @@ baseNTab = tk.Frame(notebook)
 
 notebook.add(mainTab, text='Home')
 notebook.add(baseNTab, text='BaseN')
+notebook.pack(expand=True, fill='both')
+
+announcementVar=tk.StringVar()
+def submit():
+    global matrixProc
+
+    announcement=announcementVar.get()
+    #("    ANNOUNCEMENT    " + announcement + "    ANNOUNCEMENT    ")
+
+    screenPrev.config(text=announcement)
+    announcementVar.set("")
+
+    if matrixProc:
+        matrixProc.send_signal(signal.SIGINT) # This is extremely poor practice, and should be fixed
+
+    runSubprocess(['sudo', 'python', '-m', 'matrix.utils.dispText', '-t', announcement, '-x', str(64-len(announcement)*5), '-y', str(32+6)])
+
+announcementEntry = tk.Entry(mainTab,
+                             textvariable = announcementVar,
+                             font=('DejaVu Sans',10,'normal'))
+subBtn=tk.Button(mainTab,text = 'Submit', command = submit)
+screenPrev = tk.Label(mainTab)
+
+announcementEntry.pack()
+subBtn.pack()
+screenPrev.pack()
+
 notebook.pack(expand=True, fill='both') # Expand fills space not otherwise used, fill will fill space on x and y axis
 
 exitBtn = tk.Button(mainTab,
@@ -58,11 +85,11 @@ question_menu.pack()
 def print_answers():
     displaychoice = valueofchoice.get()
     if displaychoice == "Binary":
-        return questiondisplay.config(text=f"{randBin()} ", font=("Arial", 20))
+        return questiondisplay.config(text=f"{randBin()}", font=("Arial", 20))
     elif displaychoice == "Hexadecimal":
-        return questiondisplay.config(text=f"{randHex()} ", font=("Arial", 20))
+        return questiondisplay.config(text=f"{randHex()}", font=("Arial", 20))
     elif displaychoice == "Decimal":
-        return questiondisplay.config(text=f"{randDec()} ", font=("Arial", 20))
+        return questiondisplay.config(text=f"{randDec()}", font=("Arial", 20))
 
 submit_button = tk.Button(baseNTab, text='Submit', command=print_answers) 
 submit_button.pack()
